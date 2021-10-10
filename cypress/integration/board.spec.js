3535014da4be2770778b7a7cd9d6af97b2124d01
danilotodorovic6 <danilo.todorovic@vivifyideas.com>
2021-10-10 15:54:31 +0200

    adding a column and story, editing story

--- cypress/integration/board.spec.js
+++ cypress/integration/board.spec.js
@@ -12,7 +12,7 @@ describe("board", () => {
         cy.get(loginPage.loginButton).click();
         cy.url().should("not.contain", "/login");
         cy.get(board.newBoard.addNewBoard).click().then(() => {
-            for(let i = 0; i < 5; i++){
+            for(let i = 0; i < 4; i++){
                 if(i === 0){
                     cy.get(board.newBoard.boardTitle).type(data.board.title);
                 };
@@ -23,5 +23,36 @@ describe("board", () => {
             }    
         });
         cy.url().should("contain", "/boards");
+    });
+    it("adding new column", () => {
+        cy.get(board.boardTasks.addColumn).click().type(data.board.column + "{enter}");
+    });
+    it("adding a story", () => {
+        cy.get(board.boardTasks.addTask).click({force: true});
+        cy.get(board.boardTasks.taskName).type(data.board.task);
+        cy.get(board.boardTasks.saveTask).click({force: true});
+    });
+    it("editing story", () => {
+        cy.get(board.boardTasks.createdTask).last().click();
+        //editing task title
+        cy.get(board.boardTasks.taskTitle).click();
+        cy.get(board.boardTasks.taskName).clear().type(data.board.task + "2");
+        cy.get(board.boardTasks.saveTitle).click();
+        //adding task description
+        cy.get(board.boardTasks.description).click();
+        cy.get(board.boardTasks.descriptionText).type(data.board.description);
+        cy.get(board.boardTasks.saveDescription).click();
+        //adding comment
+        cy.get(board.boardTasks.comment).click().type(data.board.comment);
+        cy.get(board.boardTasks.saveComment).click();
+        //deleting comment
+        cy.get(board.boardTasks.deleteComment).click({force: true});
+        cy.get(board.boardInfo.boardDetails.deleteBoard.saveButton).click();
+    });
+    it("delete board", () => {
+        cy.get(board.boardSidebar.configuration).click();
+        cy.get(board.boardInfo.boardDetails.deleteBoard.deleteBoardClick).click();
+        cy.get(board.boardInfo.boardDetails.deleteBoard.saveButton).click();
+        cy.get(board.newBoard.boardDeletedOKButton).click();
     });
 });
