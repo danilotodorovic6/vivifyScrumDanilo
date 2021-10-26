803e0b855c695b2c1fa95962d11648f70960ae66
danilotodorovic6 <danilo.todorovic@vivifyideas.com>
2021-10-26 16:01:41 +0200

    updated register spec with assertions

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -3,17 +3,31 @@ import loginPage from "../fixtures/login.json";
 import register from "../fixtures/sign-up.json";
 import navigationForDashboard from "../fixtures/navigationForDashboard.json";
 import data from "../fixtures/data.json";
+import errorMessages from "../fixtures/errorMessages.json";
+import signUp from "../fixtures/sign-up.json";
 
 describe("register", () => {
-    it("visit", () => {
+    before(() => {
         cy.visit("/");
+        cy.url().should("contain", "cypress.vivifyscrum-stage.com");
         cy.get(loginPage.signUpLink).click();
-        cy.get(navigationForDashboard.starterPack).first().click({force:true});
+        let elementExists = document.getElementById(navigationForDashboard.cookieGotIt);
+        if(elementExists){
+            cy.get(navigationForDashboard.cookieGotIt).click();
+        }
+        cy.get(navigationForDashboard.starterPack).click({force: true});
         cy.url().should("contain", "/sign-up");
+
     });
     it("without credentials", () => {
         cy.get(register.startYourFreeTrialButton).click();
         cy.url().should("contain", "/sign-up");
+        cy.get(signUp.forms).then(() => {
+            cy.get(signUp.errorMessages).eq(0).should("be.visible").and("have.text", errorMessages.invalidEmail);
+            cy.get(signUp.errorMessages).eq(1).should("be.visible").and("have.text", errorMessages.requiredPassword);
+            cy.get(signUp.errorMessages).eq(2).should("be.visible").and("have.text", errorMessages.requiredNumberOfUsers);
+        });
+        
     });
     it("without email", () => {
         cy.get(register.emailInput).clear();
@@ -21,6 +35,9 @@ describe("register", () => {
         cy.get(register.numberOfUsersInput).clear().type(data.registerUser.numOfUsers);
         cy.get(register.startYourFreeTrialButton).click();
         cy.url().should("contain", "/sign-up");
+        cy.get(signUp.forms).then(() => {
+            cy.get(signUp.errorMessages).eq(0).should("be.visible").and("have.text", errorMessages.invalidEmail);
+        });
     });
     it("without password", () => {
         cy.get(register.emailInput).clear().type(data.registerUser.email);
@@ -28,6 +45,9 @@ describe("register", () => {
         cy.get(register.numberOfUsersInput).clear().type(data.registerUser.numOfUsers);
         cy.get(register.startYourFreeTrialButton).click();
         cy.url().should("contain", "/sign-up");
+        cy.get(signUp.forms).then(() => {
+            cy.get(signUp.errorMessages).eq(1).should("be.visible").and("have.text", errorMessages.requiredPassword);
+        });        
     });
     it("without number of users", () => {
         cy.get(register.emailInput).clear().type(data.registerUser.email);
@@ -35,6 +55,9 @@ describe("register", () => {
         cy.get(register.numberOfUsersInput).clear();
         cy.get(register.startYourFreeTrialButton).click();
         cy.url().should("contain", "/sign-up");
+        cy.get(signUp.forms).then(() => {
+            cy.get(signUp.errorMessages).eq(2).should("be.visible").and("have.text", errorMessages.requiredNumberOfUsers);
+        });
     });
     it("with email without @", () => {
         cy.get(register.emailInput).clear().type(data.registerInvalidUser["emailWithout@"]);
@@ -42,6 +65,9 @@ describe("register", () => {
         cy.get(register.numberOfUsersInput).clear().type(data.registerUser.numOfUsers);
         cy.get(register.startYourFreeTrialButton).click();
         cy.url().should("contain", "/sign-up");
+        cy.get(signUp.forms).then(() => {
+            cy.get(signUp.errorMessages).eq(0).should("be.visible").and("have.text", errorMessages.invalidEmail);
+        });
     });
     it("with email without .", () => {
         cy.get(register.emailInput).clear().type(data.registerInvalidUser["emailWithout."]);
@@ -49,6 +75,9 @@ describe("register", () => {
         cy.get(register.numberOfUsersInput).clear().type(data.registerUser.numOfUsers);
         cy.get(register.startYourFreeTrialButton).click();
         cy.url().should("contain", "/sign-up");
+        cy.get(signUp.forms).then(() => {
+            cy.get(signUp.errorMessages).eq(0).should("be.visible").and("have.text", errorMessages.invalidEmail);
+        });
     });
     it("with existing email", () => {
         cy.get(register.emailInput).clear().type(data.user.email);
@@ -56,6 +85,9 @@ describe("register", () => {
         cy.get(register.numberOfUsersInput).clear().type(data.registerUser.numOfUsers);
         cy.get(register.startYourFreeTrialButton).click();
         cy.url().should("contain", "/sign-up");
+        cy.get(signUp.forms).then(() => {
+            cy.get(signUp.existingMail).eq(0).should("be.visible").and("have.text", errorMessages.existingEmail);
+        });
     });
     it("with password less than 5 characters", () => {
         cy.get(register.emailInput).clear().type(data.registerUser.email);
@@ -63,6 +95,10 @@ describe("register", () => {
         cy.get(register.numberOfUsersInput).clear().type(data.registerUser.numOfUsers);
         cy.get(register.startYourFreeTrialButton).click();
         cy.url().should("contain", "/sign-up");
+        cy.get(signUp.forms).then(() => {
+            cy.get(signUp.errorMessages).eq(1).should("be.visible").and("have.text", errorMessages.invalidPassword5Characters);
+        });
+
     });
     it("with number of users less than 1", () => {
         cy.get(register.emailInput).clear().type(data.registerUser.email);
@@ -70,6 +106,10 @@ describe("register", () => {
         cy.get(register.numberOfUsersInput).clear().type(data.registerInvalidUser.numOfUsersLessThan1);
         cy.get(register.startYourFreeTrialButton).click();
         cy.url().should("contain", "/sign-up");
+        cy.get(signUp.forms).then(() => {
+            cy.get(signUp.errorMessages).eq(2).should("be.visible").and("have.text", errorMessages.invalidNumberInteger);
+        });
+
     });
     it("with number of users more than 10", () => {
         cy.get(register.emailInput).clear().type(data.registerUser.email);
@@ -77,6 +117,9 @@ describe("register", () => {
         cy.get(register.numberOfUsersInput).clear().type(data.registerInvalidUser.numOfUsersMoreThan10);
         cy.get(register.startYourFreeTrialButton).click();
         cy.url().should("contain", "/sign-up");
+        cy.get(signUp.forms).then(() => {
+            cy.get(signUp.errorMessages).eq(2).should("be.visible").and("have.text", errorMessages.invalidNumberInteger);
+        });
     });
     it("with number of users not an integer", () => {
         cy.get(register.emailInput).clear().type(data.registerUser.email);
@@ -84,6 +127,9 @@ describe("register", () => {
         cy.get(register.numberOfUsersInput).clear().type(data.registerInvalidUser.numOfUsersNotInteger);
         cy.get(register.startYourFreeTrialButton).click();
         cy.url().should("contain", "/sign-up");
+        cy.get(signUp.forms).then(() => {
+            cy.get(signUp.errorMessages).eq(2).should("be.visible").and("have.text", errorMessages.invalidNumberString);
+        });
     });
     it("with valid credentials", () => {
         cy.get(register.emailInput).clear().type(data.registerUser.email);
